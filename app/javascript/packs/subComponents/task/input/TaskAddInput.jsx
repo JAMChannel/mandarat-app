a83fddd74d69eb3c3364a0904679afa50f7ac81c
JAMChannel <pon244tt@gmail.com>
2021-12-21 00:41:32 +0900

    task入力作成

--- app/javascript/packs/subComponents/task/input/TaskAddInput.jsx
+++ app/javascript/packs/subComponents/task/input/TaskAddInput.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export const TaskAddInput = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(e)
+  };
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <input className='task-add-input'
+        type="text" 
+        placeholder='taskを入力'/>
+      </form>
     </div>
   )
 }
