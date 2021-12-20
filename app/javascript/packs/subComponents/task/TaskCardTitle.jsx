c499d934855cbbd0122fec5f4c7ce505630507ad
JAMChannel <pon244tt@gmail.com>
2021-12-21 00:13:39 +0900

    Title作成

--- app/javascript/packs/subComponents/task/TaskCardTitle.jsx
+++ app/javascript/packs/subComponents/task/TaskCardTitle.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false)
+  const handleClick = () => {
+    setIsClick(true);
+    console.log(isClick)
+  };
+
+  const [inputCardTitle, setInputCardTitle] = useState("Title")
+  
+  const handleChange = (e) => {
+    console.log(inputCardTitle)
+    setInputCardTitle(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  const handleBlur = () => {
+    setIsClick(false);
+  };
+
   return (
-    <div>
+    <div onClick={handleClick} className="task-card-title">
+      {isClick ? (
+      <form onSubmit={handleSubmit}>
+        <input className="task-card-title-input"
+              type="text" 
+              onChange={handleChange} 
+              onBlur={handleBlur} 
+              value={inputCardTitle} 
+              maxLength="10"
+              autoFocus/>
+      </form>
+      ) : (
+      <h4>{inputCardTitle}</h4>
+      )}
       
     </div>
   )
