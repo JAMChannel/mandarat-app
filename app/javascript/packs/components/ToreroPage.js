00d3fb948724b663e9cba609edfba1939275178d
JAMChannel <pon244tt@gmail.com>
2021-12-20 13:33:47 +0900

    header作成

--- app/javascript/packs/components/ToreroPage.js
+++ app/javascript/packs/components/ToreroPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Header } from '../subComponents/Header'
 
 function ToreroPage() {
   return (
     <div>
-      <p>test</p>
+      <Header />
     </div>
   )
 }
---
+++ app/javascript/packs/subComponents/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+      <div className='torero-header'><h1>Jam Torero</h1></div>
+    </div>
+  )
+}
