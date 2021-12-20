ea22bae74e994bc4e4d3e18486bb975c25b76869
JAMChannel <pon244tt@gmail.com>
2021-12-20 15:42:20 +0900

    再taskcardコンポーネント作成

--- app/javascript/packs/subComponents/task/TaskCard.jsx
+++ app/javascript/packs/subComponents/task/TaskCard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { TaskCardDeleteButton } from './button/TaskCardDeleteButton'
 import { TaskAddInput } from './input/TaskAddInput'
 import { TaskCardTitle } from './TaskCardTitle'
-
 import { Tasks } from './Tasks'
 
 export const TaskCard = () => {
