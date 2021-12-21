d205e30832e4e2b5612cab09a98e7b93c3d415d6
JAMChannel <pon244tt@gmail.com>
2021-12-22 01:27:02 +0900

    DraggableID追加

--- app/javascript/packs/subComponents/task/Task.jsx
+++ app/javascript/packs/subComponents/task/Task.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Draggable } from "react-beautiful-dnd";
 
 export const Task = ({  task, taskList, setTaskList}) => {
 
@@ -9,12 +10,23 @@ export const Task = ({  task, taskList, setTaskList}) => {
 
 
   return (
-    <div className='task-box'>
-      <p className='task-box-text'>{task.text}</p>
-      <button className='task-box-trash' onClick={() => handleDelete
-      (task.id)}>
-        <i className="fas fa-trash-alt"></i>
-      </button>
-    </div>
+    <Draggable index={task.id} draggableId={task.draggableId}>
+      {(provided) => (
+        <div
+          className="task-box"
+          key={task.id}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <p className='task-box-text'>{task.text}</p>
+          <button className='task-box-trash' onClick={() => handleDelete
+          (task.id)}>
+            <i className="fas fa-trash-alt"></i>
+          </button>
+        </div>
+      )}
+
+    </Draggable>
   )
 }
