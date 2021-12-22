40ce38136cdd4091e1936e2841405daf2659d205
JAMChannel <pon244tt@gmail.com>
2021-12-22 18:01:07 +0900

    完成

--- app/javascript/packs/subComponents/task/button/TaskCardDeleteButton.jsx
+++ app/javascript/packs/subComponents/task/button/TaskCardDeleteButton.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export const TaskCardDeleteButton = () => {
+export const TaskCardDeleteButton = ({
+  taskCardsList,
+  setTaskCardsList,
+  taskCard,
+}) => {
+
+  const taskCardDeleteButton = (id) => {
+    // 削除機能追加
+    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
+  };
+  
   return (
     <div>
-      
+      <button 
+      className='task-delete-button'
+      onClick={() => taskCardDeleteButton(taskCard.id)}>
+      <i className="fas fa-times"></i>
+      </button>
     </div>
   )
 }
