0d6acc6c051dbb361a2d3ecfce392339e1392246
JAMChannel <pon244tt@gmail.com>
2022-01-01 00:29:18 +0900

    ログイン完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+
   root "homes#index"
-  
+
+
   resources :users
+  # get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 
   resources :mandarats
 
