531a40ef64cf037b294b2a8dfa21e1511d41aa6d
JAMChannel <pon244tt@gmail.com>
2022-01-01 21:48:40 +0900

    マンダラートnewページ作成

--- app/controllers/mandarats_controller.rb
+++ app/controllers/mandarats_controller.rb
@@ -3,6 +3,17 @@ class MandaratsController < ApplicationController
   end
 
   def new
+    @mandarat = Mandarat.new
+  end
+
+  def create
+    binding.pry
+    @mandarat = Mandarat.new(mandarat_params)
+    if @mandarat.save!
+      redirect_to mandarats_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -10,4 +21,12 @@ class MandaratsController < ApplicationController
 
   def edit
   end
+
+
+  private
+
+  def mandarat_params
+    # params.require(:mandarat).permit(:title ,name: []).merge(user_id: current_user.id)
+  end
+
 end
